3639332ebddc6c12f4458a59af3fe7d72da19d4c
d.trikhunkov@outlook.com <d.trikhunkov@outlook.com>
2023-02-03 20:56:29 +0300

    Добавлена функция Matrix A

--- GitApplication/Program.cs
+++ GitApplication/Program.cs
@@ -10,6 +10,23 @@ namespace GitApplication
     {
         static void Main(string[] args)
         {
+            MatrixA(10, 10);
+            Console.ReadKey();
+        }
+
+
+        public static void MatrixA(int w, int h)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Random rnd = new Random();
+            for (int i = 0; i < h; i++)
+            {
+                for (int j = 0; j < w; j++)
+                {
+                    Console.Write(rnd.Next(0, 2));
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
