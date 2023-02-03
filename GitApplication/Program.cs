b77832afbc9ce0fc580b3d2bd73c318182dae3cd
jehrzel <z3linsky@yandex.ru>
2023-02-03 21:03:25 +0300

    лол matrixB

--- GitApplication/Program.cs
+++ GitApplication/Program.cs
@@ -10,22 +10,23 @@ namespace GitApplication
     {
         static void Main(string[] args)
         {
-            
+           
         }
-
-
-        public static void MatrixA(int w, int h)
+        public static void MatrixB(int w, int h)
         {
-            Console.ForegroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Magenta;
             Random rnd = new Random();
-            for (int i = 0; i < h; i++)
+            for(int i = 0; i < w; i++)
             {
-                for (int j = 0; j < w; j++)
+                for (int j = 0; j < h; j++)
                 {
                     Console.Write(rnd.Next(0, 2));
+
                 }
+
                 Console.WriteLine();
             }
+
         }
     }
 }
